ecbac0697d3e3c09b6fdc7da6fa5ca41b29d2e39
rahulanthati12 <121819538+rahulanthati12@users.noreply.github.com>
2023-02-20 16:12:02 -0500

    created helloworld java

---
+++ helloworld.java
@@ -0,0 +1,10 @@
+ public static void main(String[] args) {
+        // object has been created for Student class
+
+        Student studentObj = new Student();
+        //student named as Rahu
+        studentObj.name = "venkat";
+        studentObj.driving();
+        studentObj.policy();
+        System.out.println("Hello Rahul" +
+                );
